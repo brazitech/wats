4f9dda2e768e0c807d32d280409d6e1ff04ca67a
webamoeba <webamoeba@4114a63b-fb60-0410-ae2b-fedb63882598>
2009-01-21 10:45:07 +0000

    Populate trunk

---
+++ components/com_waticketsystem/lang/russian.php
@@ -0,0 +1,102 @@
+<?php
+/**
+ * FileName: russian.php
+ * Date: 15.03.2007
+ * License: GNU General Public License
+ * File Version #: 1
+ * WATS Version #: 2.0.0.1
+ * Author: Vasily Korotkov (korotkov@lizardspb.ru) www.lizardspb.ru
+ **/
+
+// NAVIGATION
+DEFINE("_WATS_NAV_NEW","Новая заявка");
+DEFINE("_WATS_NAV_CATEGORY","Категория");
+DEFINE("_WATS_NAV_TICKET","Номер заявки");
+
+// USER
+DEFINE("_WATS_USER","Пользователь");
+DEFINE("_WATS_USER_SET","Пользователи");
+DEFINE("_WATS_USER_NAME","Имя");
+DEFINE("_WATS_USER_USERNAME","Имя пользователя");
+DEFINE("_WATS_USER_GROUP","Группа");
+DEFINE("_WATS_USER_ORG","Организация");
+DEFINE("_WATS_USER_ORG_SELECT","Ввод организации");
+DEFINE("_WATS_USER_EMAIL","Email");
+DEFINE("_WATS_USER_NEW","Создать нового пользователя");
+DEFINE("_WATS_USER_NEW_SELECT","Выбрать пользователя");
+DEFINE("_WATS_USER_NEW_CREATED","Создан пользователь");
+DEFINE("_WATS_USER_NEW_FAILED","У этого пользователя уже есть учетная запись");
+DEFINE("_WATS_USER_DELETED","Пользователь удалён");
+DEFINE("_WATS_USER_EDIT","Редактировать пользователя");
+DEFINE("_WATS_USER_DELETE_REC","Удалить заявки пользователей (рекоменд.)");
+DEFINE("_WATS_USER_DELETE_NOTREC","Удалить заявки пользователей и ответы на др. заявки (не рекоменд)");
+DEFINE("_WATS_USER_DELETE","Удалить пользователя");
+DEFINE("_WATS_USER_ADD","Добавить пользователя");
+DEFINE("_WATS_USER_SELECT","Выбрать пользователя");
+DEFINE("_WATS_USER_SET_DESCRIPTION","Manage Users");
+DEFINE("_WATS_USER_ADD_LIST","Были добавлены следующие пользователи");
+
+// GROUPS
+DEFINE("_WATS_GROUP_SELECT","Выберите группу");
+
+// CATEGORIES
+DEFINE("_WATS_CATEGORY","  Категория  ");
+
+// TICKETS
+DEFINE("_WATS_TICKETS_USER_OPEN","Мои открытые заявки");
+DEFINE("_WATS_TICKETS_USER_CLOSED","Мои закрытые заявки");
+DEFINE("_WATS_TICKETS_OPEN","Открытые заявки");
+DEFINE("_WATS_TICKETS_CLOSED","Закрытые заявки");
+DEFINE("_WATS_TICKETS_DEAD","Мертвые заявки");
+DEFINE("_WATS_TICKETS_OPEN_VIEW","Просмотреть все открытые заявки");
+DEFINE("_WATS_TICKETS_CLOSED_VIEW","Просмотреть все закрытые заявки");
+DEFINE("_WATS_TICKETS_DEAD_VIEW","Просмотреть все мертвые заявки");
+DEFINE("_WATS_TICKETS_NAME","  Имя заявки  ");
+DEFINE("_WATS_TICKETS_POSTS","Посты");
+DEFINE("_WATS_TICKETS_DATETIME","Последний пост");
+DEFINE("_WATS_TICKETS_PAGES","Страницы");
+DEFINE("_WATS_TICKETS_SUBMIT","Отправить новую заявку");
+DEFINE("_WATS_TICKETS_SUBMITING","Отправка заявки");
+DEFINE("_WATS_TICKETS_SUBMITTED","Отправка заявки прошла успешно");
+DEFINE("_WATS_TICKETS_DESC","  Описание  ");
+DEFINE("_WATS_TICKETS_CLOSE","Закрыть заявку");
+DEFINE("_WATS_TICKETS_CLOSED_COMP","Заявка закрыта");
+DEFINE("_WATS_TICKETS_DELETED_COMP","Заявка удалена");
+DEFINE("_WATS_TICKETS_PURGED_COMP","Заявка слита");
+DEFINE("_WATS_TICKETS_NONE","заявок не найдено");
+DEFINE("_WATS_TICKETS_FIRSTPOST","запущено: ");
+DEFINE("_WATS_TICKETS_LASTPOST","разместил: ");
+DEFINE("_WATS_TICKETS_REPLY","Ответ");
+DEFINE("_WATS_TICKETS_REPLY_CLOSE","Ответить и закрыть");
+DEFINE("_WATS_TICKETS_ASSIGN","Закрепить заявку");
+DEFINE("_WATS_TICKETS_ASSIGNEDTO","Закреплено за:");
+DEFINE("_WATS_TICKETS_ID","ID Заявки");
+DEFINE("_WATS_TICKETS_REOPEN","Открыть снова");
+DEFINE("_WATS_TICKETS_REOPEN_REASON","Опишите причину повторного открытия заявки");
+DEFINE("_WATS_TICKETS_STATE_ALL","Все");
+DEFINE("_WATS_TICKETS_STATE_PERSONAL","Персонально");
+DEFINE("_WATS_TICKETS_STATE_OPEN","Открыто");
+DEFINE("_WATS_TICKETS_STATE_CLOSED","Закрыто");
+DEFINE("_WATS_TICKETS_STATE_DEAD","Мертвые");
+DEFINE("_WATS_TICKETS_PURGE","Слить мертвые заявки в ");
+
+//MAIL
+DEFINE("_WATS_MAIL_TICKET","Заявка отправлена: ");
+DEFINE("_WATS_MAIL_REPLY","Ответ отправлен: ");
+
+//MISC
+DEFINE("_WATS_MISC_DELETE_VERIFY","Удалить меня?");
+DEFINE("_WATS_MISC_GO","Go");
+
+//ERRORS
+DEFINE("_WATS_ERROR","Произошла ошибка");
+DEFINE("_WATS_ERROR_ACCESS","У Вас недостаточно прав");
+DEFINE("_WATS_ERROR_NOUSER","Вы не авторизованы для просмотра.<br>Авторизуйтесь или свяжитесь с администратором.");
+DEFINE("_WATS_ERROR_NODATA","Форма заполнена неправильно. Попробуйте еще раз.");
+DEFINE("_WATS_ERROR_NOT_FOUND","Не найдено");
+
+//BBCODE
+DEFINE("_WATS_BB_HELP","<p><i>Наши специалисты приступят к отработке заявки в зависимости от степени срочности.</i></p> 
+
+ ");
+?>
