dcef2d089ed391ff7fbbf4ccbfef0c55b408e48a
webamoeba <webamoeba@4114a63b-fb60-0410-ae2b-fedb63882598>
2009-05-22 10:16:16 +0000

    Added alias helper

---
+++ administrator/components/com_whelpdesk/classes/helper/alias.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * @version     $Id$
+ * @package     whelpdesk
+ * @subpackage  classes
+ * @license     GNU/GPL
+ */
+
+class WAliasHelper {
+
+    static function buildAlias($name, $sympathetic = true, $replacement = '') {
+        // replace HTML entities with their actual UTF-8 character representation
+        $name = html_entity_decode($name, ENT_QUOTES, 'UTF-8');
+        
+        // check for non compliant characters
+        // # < > [ ] | { } . :
+        // and check for illegal start of name
+        // ./ ../
+        $notAllowed      = '~[\#\<\>\[\]\|\{\}\.\:]~';
+        $notAllowedStart = '~^\.\.?\/~';
+        if ($sympathetic) {
+            // replace bad characters
+            $name = preg_replace($notAllowed,      $replacement, $name)
+            $name = preg_replace($notAllowedStart, $replacement, $name)
+        } elseif (preg_match($notAllowed, $name) || preg_match(, $name)) {
+            // contains bad characters and we are not ina good mood! bad method caller bad!
+            throw new WExcpetion('DISALLOWED CHARACTERS DETECTED IN TITLE');
+        }
+        
+        // replace question marks with URL percent-encodced equivalent
+        $name = preg_replace('~\?~', '%3F', $name);
+        
+        // replace plus signs with URL percent-encodced equivalent
+        $name = preg_replace('~\+~', '%2B', $name);
+        
+        
+        // replace white-space with underscores
+        $name = preg_replace('~\s~', '_', $name);
+        
+        return $name;
+    }
+
+}
