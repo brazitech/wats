1a04e0beb015e6f271e0a64dbac4bcaa92b476d4
webamoeba <webamoeba@4114a63b-fb60-0410-ae2b-fedb63882598>
2009-06-11 09:58:25 +0000

    Added WDocumentHelper

---
+++ administrator/components/com_whelpdesk/classes/helper/document.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @version		$Id$
+ * @package		wats
+ * @package		classes
+ * @license		GNU/GPL
+ */
+
+/**
+ * Interface that all implementing classes must implement. This is used to
+ * create a generic class interface for all WDocumentHelper implementations.
+ */
+interface WDocumentHelperInterface {
+    public static function title($title, $icon = 'whelpdesk');
+    public function subtitle($subtitle);
+    public function description($description);
+}
+
+// import the current WToolbarHelper
+if (JFactory::getApplication()->isSite()) {
+    wimport('helper.document.site');
+} else {
+    wimport('helper.document.admin');
+}
