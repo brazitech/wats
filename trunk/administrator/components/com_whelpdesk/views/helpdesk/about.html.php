ca24d363663f0aee9516410199a0586612dee99d
webamoeba <webamoeba@4114a63b-fb60-0410-ae2b-fedb63882598>
2009-06-11 15:15:41 +0000

    added document metadata to about pages

--- trunk/administrator/components/com_whelpdesk/views/helpdesk/about.html.php
+++ trunk/administrator/components/com_whelpdesk/views/helpdesk/about.html.php
@@ -18,6 +18,19 @@ class HelpdeskHTMLWView extends WView {
         // let the parent do what it does...
         parent::__construct();
 	}
+
+    public function render() {
+        // add metadata to the document
+        $this->document();
+
+        // continue!
+        parent::render();
+    }
+
+    private function document() {
+        WDocumentHelper::subtitle(JText::_('ABOUT'));
+        WDocumentHelper::addPathwayItem(JText::_('ABOUT'));
+    }
     
 }
 
