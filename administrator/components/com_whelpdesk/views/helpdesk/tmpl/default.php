f6a4fec43ba8f773b99682fbbdb7ea43f4a211a7
webamoeba <webamoeba@4114a63b-fb60-0410-ae2b-fedb63882598>
2009-09-29 08:49:24 +0000

    Added Custom Fields item

--- administrator/components/com_whelpdesk/views/helpdesk/tmpl/default.php
+++ administrator/components/com_whelpdesk/views/helpdesk/tmpl/default.php
@@ -1,13 +1,13 @@
 <table class="adminform">
     <tbody>
         <tr>
-            <td width="55%" valign="top">
+            <td width="50%" valign="top" style="padding: 15px;">
                 <div id="cpanel">
                     <div style="float: left;">
                         <div class="icon">
                             <a href="<?php echo JRoute::_('index.php?option=com_whelpdesk&task=glossary.list'); ?>">
-                                <img alt="<?php echo JText::_('GLOSSARY'); ?>" src="components/com_whelpdesk/assets/icons/48-man.png"/>
-                                <span><?php echo JText::_('GLOSSARY'); ?></span>
+                                <img alt="<?php echo JText::_('WHD_GLOSSARY'); ?>" src="components/com_whelpdesk/assets/icons/48-man.png"/>
+                                <span><?php echo JText::_('WHD_GLOSSARY'); ?></span>
                             </a>
                         </div>
                     </div>
@@ -16,8 +16,8 @@
                     <div style="float: left;">
                         <div class="icon">
                             <a href="<?php echo JRoute::_('index.php?option=com_whelpdesk&task=knowledgedomains.list'); ?>">
-                                <img alt="<?php echo JText::_('KNOWLEDGE DOMAINS'); ?>" src="components/com_whelpdesk/assets/icons/48-karbon.png"/>
-                                <span><?php echo JText::_('KNOWLEDGE DOMAINS'); ?></span>
+                                <img alt="<?php echo JText::_('WHD_KD:DOMAINS'); ?>" src="components/com_whelpdesk/assets/icons/48-karbon.png"/>
+                                <span><?php echo JText::_('WHD_KD:DOMAINS'); ?></span>
                             </a>
                         </div>
                     </div>
@@ -65,6 +65,16 @@
                 <div id="cpanel">
                     <div style="float: left;">
                         <div class="icon">
+                            <a href="<?php echo JRoute::_('index.php?option=com_whelpdesk&task=fields.list'); ?>">
+                                <img alt="<?php echo JText::_('CUSTOM FIELDS'); ?>" src="components/com_whelpdesk/assets/icons/xclipboard.png"/>
+                                <span><?php echo JText::_('CUSTOM FIELDS'); ?></span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div id="cpanel">
+                    <div style="float: left;">
+                        <div class="icon">
                             <a href="">
                                 <img alt="<?php echo JText::_('WCONFIGURATION'); ?>" src="components/com_whelpdesk/assets/icons/48-Wrench.png"/>
                                 <span><?php echo JText::_('WCONFIGURATION'); ?></span>
@@ -73,7 +83,7 @@
                     </div>
                 </div>
             </td>
-            <td width="45%" valign="top">
+            <td valign="top" style="padding: 15px;">
                 <div class="pane-sliders" id="content-pane">
                     <?php
                     // get the sliders
@@ -164,22 +174,22 @@ var Site={
 				'myThirdDataset': 	[[0, 0.46], [1, 1.45], [2, 1.0], [3, 1.6]],
 				'myFourthDataset': 	[[0, 0.3], [1, 0.83], [2, 0.7], [3, 0.2]]
 			},
-			
+
 			// Define options.
 			options : {
 				// Define a padding for the canvas node
 				padding: {left: 30, right: 0, top: 10, bottom: 30},
-				
+
 				// Background color to render.
 				backgroundColor: '#f2f2f2',
-				
+
 				// Use the predefined blue colorscheme.
 				colorScheme: 'blue',
-				
+
 				// Set the labels.
 			   	xTicks: [
-					{v:0, label:'January'}, 
-			      	{v:1, label:'Februari'}, 
+					{v:0, label:'January'},
+			      	{v:1, label:'Februari'},
 			      	{v:2, label:'March'},
 			      	{v:3, label:'April'}
 				]
@@ -190,22 +200,22 @@ var Site={
 				// Add a dataset to it.
 				line.addDataset(Site.dataset);
 				// Render it.
-				line.render();		
-							
+				line.render();
+
 				// Instead of instantiating a new LineChart object,
 				// you also can use reset(), that resets the options and datasets.
 				line.reset();
-			
+
 				// Change some attributes.
 				Object.extend(Site.options,{
 					// Use the predefined red colorscheme.
 					colorScheme: 'red',
-					
+
 					// Background color to render.
 					backgroundColor: '#f2f2f2',
-					
+
 					shouldFill: false,
-					
+
 					// Set a custom colorScheme
 					colorScheme: new Hash({
 						'myFirstDataset': '#1c4a7e',
@@ -213,11 +223,11 @@ var Site={
 						'myThirdDataset': '#3a8133',
 						'myFourthDataset': '#813379'
 					}),
-					
+
 					// Set the labels.
 					xTicks: [
-						{v:0, label:'IE6'}, 
-						{v:1, label:'IE7'}, 
+						{v:0, label:'IE6'},
+						{v:1, label:'IE7'},
 						{v:2, label:'FF2'},
 						{v:3, label:'Opera 9'}
 					]
