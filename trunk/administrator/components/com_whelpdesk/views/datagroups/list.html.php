fa344050af4388c2b9b0f307a32316d53cc23872
webamoeba <webamoeba@4114a63b-fb60-0410-ae2b-fedb63882598>
2009-09-30 09:06:17 +0000

    Added datagroups view

---
+++ trunk/administrator/components/com_whelpdesk/views/datagroups/list.html.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * @version $Id$
+ * @copyright Copyright (C) James Kennard
+ * @license GNU/GPL
+ * @package helpdesk
+ */
+
+class DatagroupsHTMLWView extends WView {
+	
+    /**
+	 * Constructor
+	 */
+	public function __construct() {
+        // set the layout
+        $this->setLayout('list');
+
+        // let the parent do what it does...
+        parent::__construct();
+	}
+    
+    public function render() {
+        // populate the toolbar
+        $this->toolbar();
+
+        // add metadata to the document
+        $this->document();
+
+        // get the pagination
+        $this->pagination();
+
+        // continue!
+        parent::render();
+    }
+
+    /**
+     * Setup the toolbar
+     */
+    private function toolbar() {
+        if ($this->getModel('canCreate')) {
+            WToolbarHelper::addNew('datagroup.create.start');
+        }
+        if ($this->getModel('canEdit')) {
+            WToolbarHelper::editList('datagroup.edit.start');
+        }
+        if ($this->getModel('canDelete')) {
+            WToolbarHelper::deleteList(JText::_('ARE YOU SURE YOU WANT TO DELETE THE SELECTED DATAGROUPS?'), 'datagroup.delete.start');
+        }
+        WToolbarHelper::divider();
+        WToolbarHelper::custom(
+            'fields.list',
+            'fields',
+            'fields',
+            'WHD_CD:FIELDS',
+            false,
+            false
+        );
+        WToolbarHelper::divider();
+        WToolbarHelper::help('fields.list', true);
+
+        JFactory::getDocument()->addStyleDeclaration(
+            '.icon-32-fields {background-image:url(components/com_whelpdesk/assets/icons/xclipboard-32.png);}'
+        );
+    }
+
+    private function document() {
+        // set the subtitle
+        WDocumentHelper::subtitle(JText::_('WHD_CD:GROUPS'));
+
+        $filters = $this->getModel('filters');
+        WDocumentHelper::addPathwayItem(JText::_('WHD_CD'));
+        foreach ($filters['tables'] as $table) {
+            if (@$table->filtering) {
+                WDocumentHelper::addPathwayItem(JText::_($table->name));
+            }
+        }
+        WDocumentHelper::addPathwayItem(JText::_('WHD_CD:GROUPS'));
+    }
+}
+
+?>
